c1a995bac5d83bb77776df3f8d0587dc4420e159
盛煜强 <15157530848@163.com>
2025-07-26 22:35:44 +0800

    mybatis代码练习

---
+++ Java框架/mybatis-exercise/src/main/java/com/example/mybatisexercise/MybatisExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatisexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisExerciseApplication.class, args);
+    }
+
+}
---
+++ Java框架/mybatis-exercise/src/test/java/com/example/mybatisexercise/MybatisExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

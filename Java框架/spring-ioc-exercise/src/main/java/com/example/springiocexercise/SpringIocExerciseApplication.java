8f8183167fc8d7fae1f49fe13fd2a10f4136a3d5
盛煜强 <15157530848@163.com>
2025-07-26 22:31:44 +0800

    ioc控制翻转练习
    java后端练习

---
+++ Java框架/spring-ioc-exercise/src/main/java/com/example/springiocexercise/SpringIocExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springiocexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIocExerciseApplication.class, args);
+    }
+
+}
---
+++ Java框架/spring-ioc-exercise/src/test/java/com/example/springiocexercise/SpringIocExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springiocexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
